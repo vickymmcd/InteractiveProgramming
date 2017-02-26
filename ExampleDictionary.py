42cfb8a9bf18045c3e45ad4babe55f7f91f84397
Elepert <emily.lepert@students.olin.edu>
2017-02-26 15:11:03 -0500

    Example Dictionary
    This is an example of the output of the data class

--- ExampleDictionary.py
+++ ExampleDictionary.py
@@ -3,37 +3,37 @@
 	location1 (str) : {
 		ageRange1(str) : {
 			question1 (int): 
-				[each answer is assigned a letter (str)],
+				[each entry is the sum of each answer (ie: index 0 is the number of people who answered with the first option)],
 			question2(int): 
-				[each answer is assigned a letter (str)],
+				[each entry is the sum of each answer],
 			question3(int): 
-				[each answer is assigned a letter (str)]
+				[each entry is the sum of each answer]
 			}, 
 		ageRange2(str) : {
 			question1 (int): 
-				[each answer is assigned a letter (str)],
+				[each entry is the sum of each answer],
 			question2(int): 
-				[each answer is assigned a letter (str)],
+				[each entry is the sum of each answer],
 			question3(int): 
-				[each answer is assigned a letter (str)]
+				[each entry is the sum of each answer]
 			}
 		}, 
 	location2 (str) : {
 		ageRange1(str) : {
 			question1 (int): 
-				[each answer is assigned a letter (str)],
+				[each entry is the sum of each answer],
 			question2(int): 
-				[each answer is assigned a letter (str)],
+				[each entry is the sum of each answer],
 			question3(int): 
-				[each answer is assigned a letter (str)]
+				[each entry is the sum of each answer]
 			}, 
 		ageRange2(str) : {
 			question1 (int): 
-				[each answer is assigned a letter (str)],
+				[each entry is the sum of each answer],
 			question2(int): 
-				[each answer is assigned a letter (str)],
+				[each entry is the sum of each answer],
 			question3(int): 
-				[each answer is assigned a letter (str)]
+				[each entry is the sum of each answer]
 			}
 		}
 """
