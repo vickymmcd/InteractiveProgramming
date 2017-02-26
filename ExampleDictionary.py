135fe78562f8102a3573535a9d6c41d5771e6615
Emily Lepert <emilylepert@users-mbp.olin.edu>
2017-02-26 14:40:24 -0500

    This is an example of the output the Earthquake and Comma classes would give

---
+++ ExampleDictionary.py
@@ -0,0 +1,39 @@
+"""
+{
+	location1 (str) : {
+		ageRange1(str) : {
+			question1 (int): 
+				[each answer is assigned a letter (str)],
+			question2(int): 
+				[each answer is assigned a letter (str)],
+			question3(int): 
+				[each answer is assigned a letter (str)]
+			}, 
+		ageRange2(str) : {
+			question1 (int): 
+				[each answer is assigned a letter (str)],
+			question2(int): 
+				[each answer is assigned a letter (str)],
+			question3(int): 
+				[each answer is assigned a letter (str)]
+			}
+		}, 
+	location2 (str) : {
+		ageRange1(str) : {
+			question1 (int): 
+				[each answer is assigned a letter (str)],
+			question2(int): 
+				[each answer is assigned a letter (str)],
+			question3(int): 
+				[each answer is assigned a letter (str)]
+			}, 
+		ageRange2(str) : {
+			question1 (int): 
+				[each answer is assigned a letter (str)],
+			question2(int): 
+				[each answer is assigned a letter (str)],
+			question3(int): 
+				[each answer is assigned a letter (str)]
+			}
+		}
+"""
