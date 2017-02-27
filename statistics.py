ef99764ccfa5f86281b71f7aea02abb3d5b3cbec
Elepert <emily.lepert@students.olin.edu>
2017-02-27 14:33:15 -0500

    correct iteration through initial dictionary

--- statistics.py
+++ statistics.py
@@ -6,11 +6,11 @@ the probability that someone is from a region and their age based on that respon
 '''
 
 class Interpret:
-	def __init__(self, earthquake, commas, question, answer):
+	def __init__(self, earthquake): #, commas, question, answer):
 		self.earthquake = earthquake
-		self.commas = commas
-		self.question = question
-		self.answer = answer
+		#self.commas = commas
+		#self.question = question
+		#self.answer = answer
 
 	def reverse_dictionary(self):
 		"""
@@ -20,13 +20,23 @@ class Interpret:
 		"""
 		factors = {}
 		for i in self.earthquake: # in location of earthquake
-			for j in i: # in age range of location
-				for l in j: # in question of age 
+			location = self.earthquake[i]
+			for j in location: # in age range of location
+				age = location[j]
+				for l in age: # in question of age 
+					question = age[l]
 					a = 0
-					while a < len(l):
-						#for a in l: # for each amount of each answer
-						factors[l][a][i][j] += l[a]
+					while a < len(question):
+						
+						if a > 0:
+							#current_value = factors.get(l, {}).get(a, {}).get(i, {}).get(j, {})
+							current_value = 0
+							updated_value = current_value + self.earthquake[i][j][l][a]
+						elif a == 0:
+							current_value = 0
+						factors[l] = {a : {i: {j: current_value}}} # factors.get([l][a][i][j])
 						a +=1
+
 		print(factors)
 
 	def bayesian_update():
@@ -34,6 +44,30 @@ class Interpret:
 		Updates a prior probabilities with posterior probabilities using Bayesian
 		"""
 		pass
+
+earthquake = {'loc1' : 
+				{'age1' : 
+					{'q1' : [3,4,7,2], 
+					'q2': [4,9,3,2], 
+					'q3' : [9,2,1,0]}, 
+				'age2' : 
+					{'q1' : [3,4,7,2], 
+					'q2': [4,9,3,2], 
+					'q3' : [9,2,1,0]} 
+				},
+			'loc2' : 
+				{'age1' : 
+					{'q1' : [3,4,7,2], 
+					'q2': [4,9,3,2], 
+					'q3' : [9,2,1,0]}, 
+				'age2' : 
+					{'q1' : [3,4,7,2], 
+					'q2': [4,9,3,2], 
+					'q3' : [9,2,1,0]} 
+				}
+			}
+newdictionary = Interpret(earthquake)
+print(newdictionary.reverse_dictionary())
 
 
 
