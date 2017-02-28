79748413c445b6cee6ff3e77cd96a6ea14dc0d28
Vicky McDermott <vickymmcd@gmail.com>
2017-02-27 22:21:09 -0500

    Got final dictionary sorting working

--- data.py
+++ data.py
@@ -9,7 +9,6 @@ class Data:
         if self.type == 'earthquake':
             f = open('earthquake_data.csv')
             self.lines = f.readlines()
-            print(self.lines)
 
     def get_question(self, number):
         self.lines[0].strip('"')
@@ -22,7 +21,7 @@ class Data:
                      'Pacific': [], 'South Atlantic': [],
                      'West North Central': [], 'West South Central': []}
         for line in self.lines:
-            current_line = line.split(',')
+            current_line = line.split(';')
             key = current_line[len(current_line)-1].strip()
             if key in locations:
                 locations[str(key)].append(current_line[:len(current_line)-1])
@@ -33,20 +32,63 @@ class Data:
         locations = self.sort_by_location()
         for key in locations:
             ages = {}
-            print(locations[key])
             for line in locations[key]:
-                print(line[8])
-                print(line[:7] + line[9:])
-                if line[8] in ages:
-                    ages[line[8]].append(line[:7] + line[9:])
+                if line[7] in ages:
+                    ages[line[7]].append(line[:7] + line[8:])
                 else:
-                    ages[line[8]] = [line[:7] + line[9:]]
+                    ages[line[7]] = [line[:7] + line[8:]]
             ultimate_dict[key] = ages
         # print(ultimate_dict)
+        return ultimate_dict
+
+    def sort_answers(self):
+        best_dict = {}
+        # Get dictionary which is sorted by location and age
+        ultimate_dict = self.sort_by_age()
+        # Go through each location
+        for location in ultimate_dict:
+            # Go through each age for a specific location
+            age_dict = ultimate_dict[location]
+            nested_dict = {}
+            for age in age_dict:
+                # Access answers for specific location and age
+                agedppl = age_dict[age]
+                # Initialize list of answers for each question
+                answers = []
+                for i in range(9):
+                    answers.append([])
+                # Go through each person from that location and age group
+                for person in agedppl:
+                    # Go through that person's answers and add them to list
+                    for i, answer in enumerate(person):
+                        answers[i].append(answer)
+                # After going through each person convert list to freqdict
+                # Be sure to convert list for each question to freqdict
+                    # print(answers)
+                    new_ans_list = []
+                    for i in range(9):
+                        new_ans = self.wordListToFreqDict(answers[i])
+                        # Recreate your dictionary
+                        new_ans_list.append(new_ans)
+                # print(new_ans_list)
+                # print(age)
+                nested_dict[age] = new_ans_list
+                # print(nested_dict[age])
+            # print(nested_dict)
+            best_dict[location] = nested_dict
+        print(best_dict)
+
+    def wordListToFreqDict(self, wordlist):
+        '''Given a list of words, return a dictionary of
+         word-frequency pairs.
+         >>> wordListToFreqDict(['hi', 'hi', 'hi'])
+         {'hi': 3}
+         '''
+        wordfreq = [wordlist.count(p) for p in wordlist]
+        return dict(zip(wordlist, wordfreq))
 
     def fix_comma_issue(self):
             f = open('earthquake_data.csv', 'wt')
-            print(len(self.lines))
             f.write(self.lines[0])
             for i in range(len(self.lines)):
                 if i > 0:
@@ -64,5 +106,3 @@ class Data:
                     self.lines[i] = ''.join(my_line)
                     print(self.lines[i])
                     f.write(self.lines[i])
-            '\n'.join(self.lines)
-            # f.writelines(self.lines)
