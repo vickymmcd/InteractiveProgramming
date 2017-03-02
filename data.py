c521ef2e48f5f1f52952a29cfc9d060660685d82
Vicky McDermott <vickymmcd@gmail.com>
2017-03-01 22:17:14 -0500

    Adding more comments

--- data.py
+++ data.py
@@ -1,5 +1,8 @@
 '''
 Authors: Vicky McDermott and Emily Lepert
+
+This function pulls in and sorts data by location and age so
+that it can be interpreted.
 '''
 from pathlib import Path
 import pickle
@@ -7,17 +10,38 @@ import pickle
 
 class Data:
     def __init__(self, type):
+        '''
+        This initializes the Data class and assigns it to a type.
+        Type will either be earthquake or comma aligning with one
+        of our two data sets. It will then go on to read in the
+        data from the appropriate csv file.
+
+        type: The type of data associated with the data object
+        (earthquake or comma)
+        '''
         self.type = type
         if self.type == 'earthquake':
             f = open('earthquake_data.csv')
             self.lines = f.readlines()
 
     def get_question(self, number):
+        '''
+        This function allows you to get the question of a given number.
+
+        number: Number of the question you want returned.
+        returns: Text of question of number 'number'
+        '''
         self.lines[0].strip('"')
         ques = [line.strip('"') for line in self.lines[0].split('","')]
         return ques[number]
 
     def sort_by_location(self):
+        '''
+        Sorts the data by location from where people come.
+
+        returns: Dictionary with locations as keys and lists of
+        answers given by people from those locations as values.
+        '''
         locations = {'East North Central': [], 'East South Central': [],
                      'Middle Atlantic': [], 'Mountain': [], 'New England': [],
                      'Pacific': [], 'South Atlantic': [],
@@ -30,6 +54,14 @@ class Data:
         return locations
 
     def sort_by_age(self):
+        '''
+        Sorts the data by age and location.
+
+        returns: Nested dictionary with locations as keys to
+        first dictionary and ages as keys to the nested dictionaries.
+        Values are lists of all answers from all people from that
+        age and location.
+        '''
         ultimate_dict = {}
         locations = self.sort_by_location()
         for key in locations:
@@ -44,6 +76,10 @@ class Data:
         return ultimate_dict
 
     def sort_answers(self):
+        """
+        Sorts the data by questions and numbers of answers for
+        each age/location
+        """
         my_file = Path('earthquake_dict.pickle')
         if my_file.is_file():
             # Load data from a file
@@ -93,26 +129,34 @@ class Data:
         return dict(zip(wordlist, wordfreq))
 
     def fix_comma_issue(self):
-            f = open('earthquake_data.csv', 'wt')
-            f.write(self.lines[0])
-            for i in range(len(self.lines)):
-                if i > 0:
-                    print(i)
-                    my_line = list(self.lines[i])
-                    for i, letter in enumerate(my_line):
-                        if letter == ',':
-                            if my_line[i+1] == ' ':
-                                pass
-                            elif my_line[i+1] == '0' or my_line[i+1] == '9':
-                                pass
-                            else:
-                                my_line[i] = ';'
-                                print('fixing a comma')
-                    self.lines[i] = ''.join(my_line)
-                    print(self.lines[i])
-                    f.write(self.lines[i])
+        """
+        Separates things with semicolons so that regular commas are
+        differentiated.
+        """
+        f = open('earthquake_data.csv', 'wt')
+        f.write(self.lines[0])
+        for i in range(len(self.lines)):
+            if i > 0:
+                print(i)
+                my_line = list(self.lines[i])
+                for i, letter in enumerate(my_line):
+                    if letter == ',':
+                        if my_line[i+1] == ' ':
+                            pass
+                        elif my_line[i+1] == '0' or my_line[i+1] == '9':
+                            pass
+                        else:
+                            my_line[i] = ';'
+                            print('fixing a comma')
+                self.lines[i] = ''.join(my_line)
+                print(self.lines[i])
+                f.write(self.lines[i])
 
     def get_data(self, location, age, question):
+        """
+        Gets number of times each answer showed up for the given
+        question among people from the given location and age.
+        """
         best_dict = self.sort_answers()
         age_dict = best_dict[location]
         answers = age_dict[age]
