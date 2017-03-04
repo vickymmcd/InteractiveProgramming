f64afbcb1df3d33fe4ea0b6a1ccbcdba9e2d3493
Vicky McDermott <vickymmcd@gmail.com>
2017-03-04 17:47:06 -0500

    Starting class things, getting colors going

--- example.py
+++ example.py
@@ -1,12 +1,16 @@
-from bokeh.plotting import output_file, figure, save
+from bokeh.plotting import output_file, figure, save, show
+from bokeh.layouts import column
+from bokeh.models import CategoricalColorMapper, ColumnDataSource
 import shapefile
 
-sf = shapefile.Reader("cb_2015_us_state_5m.shp")
+sf = shapefile.Reader("StateBorders/cb_2015_us_state_5m.shp")
 shapes = sf.shapes()
 print(len(shapes))
 x_coords = []
 y_coords = []
-p = figure(title='My State', plot_width=1200, plot_height=700,
+mapper = CategoricalColorMapper(factors=[40, 50, 60, 70],
+                                palette=['red', 'green', 'blue', 'yellow'])
+p = figure(title='Your Location', plot_width=1200, plot_height=700,
            x_range=(-130, -60), y_range=(24, 50))
 for i, shape in enumerate(shapes):
     x_coords.append([])
@@ -15,5 +19,14 @@ for i, shape in enumerate(shapes):
         x_coords[i].append(point[0])
         y_coords[i].append(point[1])
 p.patches(x_coords, y_coords, line_color='red')
-outputpf = r"map.html"
-save(obj=p, filename=outputpf)
+p2 = figure(title='Your Age', plot_height=200, plot_width=700)
+x_coords = [[18, 18, 30, 30], [30, 30, 45, 45], [45, 45, 60, 60],
+            [60, 60, 75, 75]]
+y_coords = [[0, 5, 5, 0], [0, 5, 5, 0], [0, 5, 5, 0], [0, 5, 5, 0]]
+source = ColumnDataSource(data={'x_coords': x_coords, 'y_coords': y_coords,
+                                'probs': [40, 50, 60, 70]})
+p2.patches(x_coords, y_coords, source=source, line_color='red',
+           color={'field': 'probs', 'transform': mapper})
+output_file('map.html')
+layout = column(p, p2)
+show(layout)
--- map.py
+++ map.py
@@ -3,16 +3,25 @@ Authors: Vicky McDermott and Emily Lepert
 
 This function represents the visual Map object.
 '''
-import geoplotlib
-from geoplotlib.utils import read_csv, BoundingBox
+from bokeh.plotting import figure
+import shapefile
 
 
 class Map:
     def __init__(self):
-        self.map = map
+        self.figure = figure(title='Your Location', plot_width=1200,
+                             plot_height=700, x_range=(-130, -60),
+                             y_range=(24, 50))
+        self.x_coords = []
+        self.y_coords = []
+        self.sf = shapefile.Reader("StateBorders/cb_2015_us_state_5m.shp")
+        self.shapes = self.sf.shapes()
+        for i, shape in enumerate(self.shapes):
+            self.x_coords.append([])
+            self.y_coords.append([])
+            for point in shape.points:
+                self.x_coords[i].append(point[0])
+                self.y_coords[i].append(point[1])
 
-    if __name__ == '__main__':
-        data = read_csv('us-state-capitals.csv')
-        geoplotlib.hist(data, colorscale='sqrt', binsize=60)
-        geoplotlib.set_bbox(BoundingBox.USA)
-        geoplotlib.show()
+    def update_map(self):
+        self.figure.patches(self.x_coords, self.y_coords, line_color='red')
