27d5d2c5c2c3349e200c2a217982e704d5c64429
Vicky McDermott <vickymmcd@gmail.com>
2017-03-04 22:36:26 -0500

    made regions for map and made it an object

--- map.py
+++ map.py
@@ -3,7 +3,8 @@ Authors: Vicky McDermott and Emily Lepert
 
 This function represents the visual Map object.
 '''
-from bokeh.plotting import figure
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import CategoricalColorMapper, ColumnDataSource
 import shapefile
 
 
@@ -14,6 +15,20 @@ class Map:
                              y_range=(24, 50))
         self.x_coords = []
         self.y_coords = []
+        self.regions = [8, 4, 0, 8, 6, 2, 1, 7, 1, 9, 3, 8, 9, 2, 7, 1, 8, 4,
+                        1, 5, 8, 8, 0, 7, 5, 4, 4, 7, 4, 9, 5, 7, 0, 0, 5, 7,
+                        7, 5, 5, 0, 0, 0, 0, 6, 6, 3, 4, 3, 9, 2, 4, 1, 2, 7,
+                        4, 8]
+        self.mapper = CategoricalColorMapper(factors=[0, 1, 2, 3, 4, 5, 6, 7,
+                                                      8], palette=['gray',
+                                                                   'red',
+                                                                   'yellow',
+                                                                   'blue',
+                                                                   'green',
+                                                                   'orange',
+                                                                   'brown',
+                                                                   'violet',
+                                                                   'black'])
         self.sf = shapefile.Reader("StateBorders/cb_2015_us_state_5m.shp")
         self.shapes = self.sf.shapes()
         for i, shape in enumerate(self.shapes):
@@ -22,6 +37,25 @@ class Map:
             for point in shape.points:
                 self.x_coords[i].append(point[0])
                 self.y_coords[i].append(point[1])
+        self.source = ColumnDataSource(data={'x_coords': self.x_coords,
+                                             'y_coords': self.y_coords,
+                                             'region': self.regions})
 
-    def update_map(self):
-        self.figure.patches(self.x_coords, self.y_coords, line_color='red')
+    def update_map(self, probs=[]):
+        self.figure.patches(self.x_coords, self.y_coords, source=self.source,
+                            line_color='red', color={'field': 'region',
+                                                     'transform': self.mapper})
+        self.source = ColumnDataSource(data={'x_coords': self.x_coords,
+                                             'y_coords': self.y_coords,
+                                             'region': self.regions,
+                                             'probs': probs})
+
+    def show_the_map(self):
+        self.update_map()
+        output_file('map.html')
+        show(self.figure)
+
+
+if __name__ == '__main__':
+    map = Map()
+    map.show_the_map()
