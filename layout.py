ec564ab0a94bfe0f8f913126d7f1ac0558dff220
Vicky McDermott <vickymmcd@gmail.com>
2017-03-06 17:51:41 -0500

    updating layout

--- layout.py
+++ layout.py
@@ -14,25 +14,27 @@ from bokeh.plotting import output_file, show
 
 
 class Layout:
-    def __init__(self, question_index, data, data_type):
+    def __init__(self, question_index, data, data_type, age_probs,
+                 region_probs):
         self.map = Map()
         self.ageline = AgeLine()
         self.scaleline = ScaleLine()
         self.question = Question(question_index, data, data_type)
-        self.layout = row(column(self.map.get_fig([0, .11, .11, .11, .11,
-                                               .11, .11, .11, .11, .11]),
-                             self.ageline.get_fig([.25, .25, .25, .25]),
-                             self.scaleline.get_fig()), self.question.get_fig())
+        self.layout = row(column(self.map.get_fig(region_probs),
+                                 self.ageline.get_fig(age_probs),
+                                 self.scaleline.get_fig()),
+                          self.question.get_fig())
 
     def show_layout(self):
         output_file('layout.html')
         show(self.layout)
 
 
-
 if __name__ == '__main__':
     data = Data('comma')
     prior = [0.02777] * 36
-    newdictionary1 = Interpret(data, prior, 2, "No", "comma")
+    newdictionary1 = Interpret(data, prior, 2, "No", "comma",
+                               [.25, .25, .25, .25], [0, .11, .11, .11, .11,
+                                                      .11, .11, .11, .11, .11])
     layout = Layout(1, newdictionary1, "comma")
     layout.show_layout()
